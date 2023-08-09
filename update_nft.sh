9b9962075a24ac808e4d52305eff783835d2a0f7
wy580477 <98247050+wy580477@users.noreply.github.com>
2023-08-09 16:46:58 +0800

    Add nft version of script

---
+++ update_nft.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+#set -x
+
+# Natter/NATMap
+private_port=$4 # Natter: $3; NATMap: $4
+public_port=$2 # Natter: $5; NATMap: $2
+
+# qBittorrent.
+qb_addr_url="http://localhost:8080" 
+#qb_ip_addr="192.168.1.2" # Only needed when qbit runs on a different host
+qb_username="admin"
+qb_password="adminadmin"
+
+echo "Update qBittorrent listen port to $public_port..."
+
+# Update qBittorrent listen port.
+qb_cookie=$(curl -s -i --header "Referer: $qb_addr_url" --data "username=$qb_username&password=$qb_password" $qb_addr_url/api/v2/auth/login | grep -i set-cookie | cut -c13-48)
+curl -X POST -b "$qb_cookie" -d 'json={"listen_port":"'$public_port'"}' "$qb_addr_url/api/v2/app/setPreferences"
+
+echo "Update nftables..."
+
+# Use nftables to forward traffic.
+nft delete table qbit_redirect
+nft add table ip qbit_redirect
+nft 'add chain ip qbit_redirect prerouting { type nat hook prerouting priority 0; }'
+
+if [ "$qb_ip_addr" = "" ];then
+    nft add rule ip qbit_redirect prerouting tcp dport $private_port redirect to :$public_port
+else
+    nft add rule ip qbit_redirect prerouting tcp dport $private_port dnat to $qb_ip_addr:$public_port
+fi
+
+echo "Done."
