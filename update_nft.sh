545255114b3a63516a7117615c2430172428b177
Arena Druid <95113209+ArenaDruid@users.noreply.github.com>
2023-10-05 13:29:01 +0800

    change nftables rules to suit udp

--- update_nft.sh
+++ update_nft.sh
@@ -23,12 +23,20 @@ echo "Update nftables..."
 # Use nftables to forward traffic.
 nft delete table qbit_redirect
 nft add table inet qbit_redirect
-nft 'add chain inet qbit_redirect prerouting { type nat hook prerouting priority -100; }'
+nft 'add chain inet qbit_redirect prerouting { type nat hook prerouting priority -100; }' 
 
 if [ "$qb_ip_addr" = "" ];then
     nft add rule inet qbit_redirect prerouting tcp dport $private_port redirect to :$public_port
+    # redirect the udp
+    nft add rule inet qbit_redirect prerouting udp dport $private_port redirect to :$public_port
+    # set the rules allow allow tcp and udp into the public_port
+    nft 'add chain inet qbit_redirect INPUT { type fliter hook input priority 0; policy drop;}'
+    nft add rule inet qbit_redirect INPUT tcp dport $public_port accept
+    nft add rule inet qbit_redirect INPUT udp dport $public_port accept
 else
     nft add rule inet qbit_redirect prerouting tcp dport $private_port dnat to $qb_ip_addr:$public_port
+    # redirect the udp
+    nft add rule inet qbit_redirect prerouting udp dport $private_port dnat to $qb_ip_addr:$public_port
 fi
 
 echo "Done."
